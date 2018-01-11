102cf25199772ce6cbf55c2003457e1635ce201c
Phil Dier <phil@raventools.com>
2018-01-11 12:53:44 -0600

    add history endpoint to crawl session

--- src/RavenTools/SiteAuditorSdk/Resources/CrawlSession.php
+++ src/RavenTools/SiteAuditorSdk/Resources/CrawlSession.php
@@ -7,6 +7,7 @@ class CrawlSession extends Common {
 	protected $get_endpoint = "crawl_sessions/{id}";
 	protected $delete_endpoint = "crawl_sessions/{id}";
 	protected $previous_endpoint = "crawl_sessions/{id}/previous";
+	protected $history_endpoint = "crawl_sessions/history/{site_id}";
 	protected $response_key = 'crawl';
 
 	public function previous($resource_id = null) {
@@ -19,6 +20,17 @@ class CrawlSession extends Common {
 		];
 
 		$uri = $this->substitute($this->previous_endpoint, $params);
+		$response = $this->client->request('GET', $uri);
+
+		return $this->client->decode($response);
+	}
+
+	public function history($resource_id) {
+		$params = [
+			'site_id' => $resource_id,
+		];
+
+		$uri = $this->substitute($this->history_endpoint, $params);
 		$response = $this->client->request('GET', $uri);
 
 		return $this->client->decode($response);
